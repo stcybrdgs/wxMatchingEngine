689884a1321151f46de9138a02f8e49a54564eed
Stacy Bridges <stcybrdgs@gmail.com>
2019-06-15 14:49:41 -0500

    git commit

--- processor/preprocessor.py
+++ processor/preprocessor.py
@@ -25,14 +25,12 @@ import unicodedata2
 from spacy.lang.en.examples import sentences
 from spacy.lang.en.stop_words import STOP_WORDS
 
-
 # GLOBALS  =========================================
 # define special characters to be removed by the string cleaner
 # rem periods are important, please do not remove them
 special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
                 '{', '}', '[', ']', '|', '\\', ':', ';', '\"', '\'',
                 '<', ',', '>', '?', '/']
-
 
 # FUNCTIONS  =======================================
 def string_cleaner(d):
@@ -44,7 +42,6 @@ def string_cleaner(d):
     # d = lemmatizer(d)
     # porterStemmer
     return d
-
 
 # remove accents from a doc / string
 # receive doc obj as d and return processed obj
@@ -58,24 +55,20 @@ def remove_accents(d):
     d = d.decode("utf-8")
     return str(d)
 
-
 # perform look-up based lemmatization
 # rem to provide a lookup lemmatizer for your language, import the lookup
 # table and add it to the Language class as lemma_lookup:
 def lemmatizer(d):
     pass
 
-
 def normalizer(d):
     d = d.lower()
     return d
-
 
 # Reduce the string token to its stem and return root word
 def porterStemmer(s):
     # print('Porter Stemmer...')
     return jellyfish.porter_stem(s)
-
 
 # remove special characters from the doc object
 # and return to caller
@@ -85,13 +78,11 @@ def remove_special_chars(d):
             d = d.replace(char, ' ')
     return d
 
-
 # remove leadin, trailing, and duplicative whitespace
 def remove_whitespace(d):
     d = d.strip()  # remove leading and trailing whitespace
     d = re.sub(' +', ' ', d)  # remove duplicative whitespace
     return d
-
 
 # remove stop words from doc object
 # rem stop words are most common words that are useful to filter out
