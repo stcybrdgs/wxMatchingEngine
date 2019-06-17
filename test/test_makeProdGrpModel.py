1579c8b914cae70759947dbc53dd6ceff8e94787
Stacy Bridges <stcybrdgs@gmail.com>
2019-06-16 23:45:06 -0500

    git commit

--- test/test_makeProdGrpModel.py
+++ test/test_makeProdGrpModel.py
@@ -1,20 +1,63 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jun 16
-@author: Stacy
+@author: Stacy Bridges
+
+these scripts test the functions needed to create an annotated
+nlp object from a csv product group. the pumps group is used for this code set.
+
+importing:  ------------------
+    the csv is imported twice:
+    on the first import, the data is captured in list format so that the
+    arrays can be used to capture:
+        - productID
+        - product
+        - supplier
+        - mpn
+
+    on the second import, the data is captured in string format as a text
+    object that can be turned into an nlp object.
+
+custom tagging:    ------------
+after the nlp object is created, the array data is used to create custom
+annotations for the corresponding tokens/spans. then, the tokens/spans for
+product and supplier are used to locate the strings that need to be further
+annotated with phonetic encodings for soundex and nysiis.
+
+storing:    -------------------
+the final version of the nlp object is saved as a pickle so that it can
+be imported into the match engine when necessary to compare against new
+data for the use cases (1) erp vs mdm; and (2) tender vs erp
 
 """
-# IMPORTS  =========================================
+# IMPORT LIBS  ======================================
+import os
+import sys
 import csv
 import spacy
 from spacy.lang.en.examples import sentences
+
+# PATHS ================================
+# import importlib
+# rem use importlib to import module stored as string
+sys.path.append('../io/')
+sys.path.append('../ners/')
+sys.path.append('../preprocessor/')
+sys.path.append('../processor/')
+sys.path.append('../store/')
+
+# IMPORT FUNCTIONS  =====================
+#import loader
+#import processor
+import preprocessor
 
 # FUNCTIONS  =======================================
 
 def main():
     nlp = spacy.load('en_core_web_sm')
 
-    #txt_file = input('../store/model/erp10/pumps/prod_pumps_erp10.txt')
+    '''
+    # get product group data file and feed the info into a text object
     csv_path = '../store/model/erp10/pumps/prod_pumps_erp10.csv'
     txt_obj = ''
     with open (csv_path) as data:
@@ -23,10 +66,17 @@ def main():
         for row in reader:
             txt_obj = txt_obj + ' '.join(row) + '\n'
 
+    # clean the text object
+    txt_obj = preprocessor.string_cleaner(txt_obj)
     print(txt_obj)
 
+    # create the nlp object:
     pumps_erp10 = nlp(txt_obj)
 
+
+    for token in pumps_erp10:
+        print(token.text)
+    '''
 
     '''
     with open(txt_file, "w") as my_output_file:
