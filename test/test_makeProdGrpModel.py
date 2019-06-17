4bd9ffd0997125265c50275d1fb67a1a66ea0bea
Stacy Bridges <stcybrdgs@gmail.com>
2019-06-17 00:17:59 -0500

    git commit

--- test/test_makeProdGrpModel.py
+++ test/test_makeProdGrpModel.py
@@ -30,6 +30,7 @@ be imported into the match engine when necessary to compare against new
 data for the use cases (1) erp vs mdm; and (2) tender vs erp
 
 """
+
 # IMPORT LIBS  ======================================
 import os
 import sys
@@ -56,57 +57,8 @@ import preprocessor
 def main():
     nlp = spacy.load('en_core_web_sm')
 
-    '''
+    # import csv -- first pass
     # get product group data file and feed the info into a text object
-    csv_path = '../store/model/erp10/pumps/prod_pumps_erp10.csv'
-    txt_obj = ''
-    with open (csv_path) as data:
-        reader = csv.reader(data, delimiter =' ', quotechar = ' ',
-                            quoting = csv.QUOTE_MINIMAL)
-        for row in reader:
-            txt_obj = txt_obj + ' '.join(row) + '\n'
-
-    # clean the text object
-    txt_obj = preprocessor.string_cleaner(txt_obj)
-    print(txt_obj)
-
-    # create the nlp object:
-    pumps_erp10 = nlp(txt_obj)
-
-
-    for token in pumps_erp10:
-        print(token.text)
-    '''
-
-    '''
-    with open(txt_file, "w") as my_output_file:
-        my_output_file.write("#1\n")
-        my_output_file.write("double({},{})\n".format(len(text_list), 2))
-        for line in text_list:
-            my_output_file.write("  " + line)
-        print('File Successfully written.')
-    '''
-    '''
-    with open('../store/model/erp10/pumps/prod_pumps_erp10.csv') as data:
-        docObj = csv.reader(data, delimiter='|')
-        for row in docObj:
-            print(row)
-
-    # create model ------------------
-    # get doc
-    '''
-    '''
-    with open('../store/model/erp10/pumps/prod_pumps_erp10.csv') as infile:
-        fileObj = csv.reader(infile, delimiter='|')
-        doc = ''
-        for row in fileObj:
-            print(row)
-    '''
-    '''
-    csv_reader = csv.DictReader(csv_file)
-    '''
-    '''
-    data = ''
     with open('../store/model/erp10/pumps/prod_pumps_erp10.csv') as data:
         data = csv.reader(data, delimiter='|')
         headers = []
@@ -114,6 +66,10 @@ def main():
         products = []
         suppliers = []
         mpns = []
+
+        # TEST list
+        testList = [headers, productIDs,
+                    products, suppliers, mpns]
         i = 0
         for row in data:
             if i == 0:
@@ -129,25 +85,9 @@ def main():
                 suppliers.append(supplier)
                 mpns.append(mpn)
             i += 1
-    print(headers)
-    print(productIDs)
-    '''
-    '''
-    print('csv_reader: \n')
-    for row in csv_reader:
-        print(row)
-    print('headers: \n', headers)
-    '''
-    '''
-    # clean the test string
-    doc = preprocessor.string_cleaner(doc)
-    print('/nstring clean doc:/n', doc)
-
-    # create nlp object from test string
-    processor.create_nlp_object(doc)
-    '''
-
-
+    # TEST print
+    for item in testList:
+        print(item)
 
     print('Done.')
 
