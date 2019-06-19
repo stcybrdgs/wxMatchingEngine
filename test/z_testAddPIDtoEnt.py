c0f298dc2e9f2e4d2864d228c42ad18689787581
Stacy Bridges <stcybrdgs@gmail.com>
2019-06-18 19:47:17 -0500

    git commit

--- test/z_testAddPIDtoEnt.py
+++ test/z_testAddPIDtoEnt.py
@@ -3,12 +3,13 @@
 Created on Tue Jun 18
 @author: Stacy Bridges
 
-script to make custom sentence segmenter
+script to test adding ent tag to product ids
 """
 
 # IMPORT LIBS  ======================================
 import spacy
 from spacy.tokens import Span
+from spacy.strings import StringStore
 import csv
 import sys
 import os
@@ -24,33 +25,6 @@ global product_ids
 product_ids = []
 
 # CUSTOM PIPES  =========================
-def field_val_tagger():
-    global product_ids
-
-    PRODUCTID = nlp_obj_cln.vocab.strings[u"PRODUCTID"]
-    pid = ''
-    for token in nlp_obj:
-        i = 0
-        for i in product_ids:
-            if token.text == product_ids[i]
-                pid = product_ids[i]
-                nlp_obj_cln.ents = list(nlp_obj_cln.ents) + pid
-
-def custom_sentencizer(doc):
-    pass
-    '''
-    for i, token in enumerate(doc[:-2]):
-        # Define sentence start if pipe + titlecase token
-        if token.text == "|" and doc[i+1].is_title:
-            doc[i+1].is_sent_start = True
-        else:
-            # Explicitly set sentence start to False otherwise, to tell
-            # the parser to leave those tokens alone
-            doc[i+1].is_sent_start = False
-    '''
-
-    return doc
-
 def common_key_tagger(doc):
     #print("After tokenization, this doc has {} tokens.".format(len(doc)))
     #print("The part-of-speech tags are:", [token.pos_ for token in doc])
@@ -59,6 +33,10 @@ def common_key_tagger(doc):
         pass
     return doc
 
+def custom_sentencizer(doc):
+    pass
+    return doc
+# TO DO:
 # tagger: lu token vs span vs ent -- which tag to use for which data?
 #   Product ID, Product, Supplier, MfrPartNo, Description
 #       ent: PRODUCT (token level), tok: soundex, tok NYSIIS
@@ -81,7 +59,7 @@ def main():
     # tokenizer -> tagger -> custom_sentencizer -> ner -> common_key_tagger
     # consider adding: entity_ruler, merge_noun_chunks (https://spacy.io/usage/processing-pipelines/)
     nlp.add_pipe(custom_sentencizer, before="tagger")  # Insert before the parser
-    nlp.add_pipe(field_val_tagger, name = "field_val_tagger", last=True)
+    #nlp.add_pipe(field_val_tagger, name = "field_val_tagger", last=True)
     nlp.add_pipe(common_key_tagger, name="common_key_tagger", last=True)
 
     print(nlp.pipe_names)  # test print
@@ -97,7 +75,10 @@ def main():
             if i == 0:
                 header.append(row)
             else:
-                product_id = row[0]
+                # rem string.cleaner normalized all text in text obj
+                # so all metadata must also be normalized to enable
+                # matching
+                product_id = row[0].lower()
                 product_ids.append(product_id)
             # create text object with '.' at end
             if i != 0:
@@ -122,37 +103,53 @@ def main():
         i += 1
 
 
-    # observations:
-    # if you don't clean the txt_obj, spacy sees all records as one sentence
 
-    # TEST PRINT  -----------------------
-    # print('\n\ntxt_obj after cleaning:\n', txt_obj)  # test print
-
-    # create the nlp object:
-    print('\n\ntxt_obj:')
-    i = 0
-    for sent in nlp_obj.sents:
-        print(sent.text, '**end**\n\n')
-        i += 1
-
-    # TEST print text attributes  -----------------------
+    #--------------------------------------
+    # testing: setting entity annotations
+    #--------------------------------------
     '''
-    i = 0
-    for sent in nlp_obj.sents:
-        print(sent.text, '**end**\n\n')
-        i += 1
-    #print('\n\n# of sents: ', i)  # test print
-    d = ' | '
-    # text attributes
-    attr = [ 'i','text','lemma_','tag_','ent_type_','ent_iob_',
-            'ent_id_','is_alpha','is_digit','is_punct','is_space',
-            'like_num']
+    # find token positions
+    print('\n\nfind token positions:\n')
     for token in nlp_obj:
-        print(token.text, token.i, token.tag_, token.pos_)
-        print(token.ent_type_, token.ent_iob_, token.ent_id_)
-        print(token.is_alpha, token.is_digit, token.is_punct)
-        print(token.is_space, token.like_num)
-        print('\n\n')
+        print(token.text, token.i)
+
+    PRODUCT = nlp_obj.vocab.strings[u"PRODUCT"]
+    # get hash value of entity label
+    igp = Span(nlp_obj, 1, 4, label=PRODUCT) # create a Span for the new entity
+    nlp_obj.ents = list(nlp_obj.ents) + [igp]
     '''
+
+
+
+
+    # STOPPED HERE
+    PRODUCTID = nlp_obj.vocab.strings[u"PRODUCTID"]
+    for token in nlp_obj:
+        i = 0
+        for id in product_ids:
+            if token.text == id:
+                start = token.i
+                end = token.nbor().i
+                print('token: {}, token start: {}, token end: {}'.format(token.text, start, end))
+
+                pid = Span(nlp_obj, start, end, label=PRODUCTID)
+                nlp_obj.ents = list(nlp_obj.ents) + [pid]
+
+                i += 1
+
+    #print('found{} product ids.\n'.format(found))
+
+    print('\nAfter')
+    ents = [(e.text, e.start_char, e.end_char, e.label_) for e in nlp_obj.ents]
+
+    for e in nlp_obj.ents:
+        print(e.text, e.label_)
+
+    print('\n\n')
+    '''
+    for token in nlp_obj:
+        print(token.text, token.tag_)
+    '''
+
 
 if __name__ == '__main__' : main()
