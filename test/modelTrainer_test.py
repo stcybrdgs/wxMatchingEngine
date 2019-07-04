28d11180dde1bc88253a2cf94132f6bc2edee1e1
Stacy Bridges <stcybrdgs@gmail.com>
2019-07-04 11:05:15 -0500

    git commit

---
+++ test/modelTrainer_test.py
@@ -0,0 +1,31 @@
+# model trainer test
+# LIB IMPORTS =================================
+import spacy
+import sys
+import os
+
+# IMPORT PATHS ================================
+sys.path.append('preprocessor/')
+
+# .PY IMPORTS =================================
+import loader
+import string_cleaner
+
+# GLOBALS  ====================================
+
+# FUNCTIONS  ==================================
+
+# MAIN  =======================================
+def main():
+    # setup pipeline
+
+    # load file
+
+    # create nlp object
+
+    # train nlp obj
+
+    # end program
+    print('Done.')
+
+if '__name__==__main__' : main()
