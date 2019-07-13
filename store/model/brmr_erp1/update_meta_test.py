83fdb0a0475cf07578905130040577d8588dc25c
Stacy Bridges <stcybrdgs@gmail.com>
2019-07-13 13:23:40 -0500

    git commit

--- store/model/brmr_erp1/update_meta_test.py
+++ store/model/brmr_erp1/update_meta_test.py
@@ -7,3 +7,19 @@
 Friday, July 12, 2019
 Stacy Bridges
 '''
+import json
+
+json_content = []
+nu_pipeline = ['tagger', 'entity_ruler', 'ner']
+
+# read in meta.json
+with open('model_entRuler/meta.json','r') as jsonfile:
+    json_content = json.load(jsonfile)
+    old_pipeline = json_content['pipeline']
+
+# update json_content to reflect new pipeline
+json_content['pipeline'] = nu_pipeline
+
+# write the new pipeline to the json file
+with open('model_entRuler/meta.json','w') as jsonfile:
+    json.dump(json_content, jsonfile)
