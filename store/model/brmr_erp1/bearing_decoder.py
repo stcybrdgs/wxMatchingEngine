45c7c24075cd4a05ac1f6c09c6b85cdcd347dc76
Stacy Bridges <stcybrdgs@gmail.com>
2019-07-17 11:29:25 -0500

    git commit

--- store/model/brmr_erp1/bearing_decoder.py
+++ store/model/brmr_erp1/bearing_decoder.py
@@ -9,10 +9,10 @@ a bearing decoder
 '''
 
 # GLOBALS  ---------------------------------------------------------
-bearing_code = [
+bearing_codes = [
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', 'C', 'N', 'QJ', 'T'
 ]
-bearing_type = [
+bearing_types = [
 	'Double row angular contact ball bearing',
 	'Self-aligning ball bearing',
 	'Spherical roller bearing',  # may also be thrust bearing
@@ -28,41 +28,53 @@ bearing_type = [
 	'Tapered roller bearing',  # in accordance with ISO 355'
 ]
 
-
 # MAIN  ------------------------------------------------------------
 def main():
 	# 6207 FAF light deep groove ball bearing 85 mm
 	# 6006 SKF extra light deep groove ball bearing 75mm
 	manuf = ['FAF', 'SKF']
-	mMat = ['6207', '6006']
+	mMat = ['3207', '3206']
 
 	i = 0
 	for item in manuf:
-		if manuf = 'SKF':
-			if len(mMat[i]) == 4:
-				pass
-			if len(mMat[i]) == 5:
-		prod_str = ''
-		att_str = ''
+		lastTwo = ''
+		bearing_attr = ''
+		bearing_code = ''
+		bearing_type = ''
+		d = ''  # diameter
 
-		# get product string  ----------------
+		if item == 'SKF':
+			# get bearing type
+			if mMat[i][0] == '3' and len(item) == 4:
+				# get the bearing code
+				bearing_code = '0'
 
+			j = 0
+			index = 0
+			# use bearing code to get array index
+			for code in bearing_codes:
+				if code == bearing_code:
+					index = j
+				j += 1
 
+			# use the array index to get the bearing type
+			bearing_type = bearing_types[index]
 
-		# get attribute string -----------------
+			# get the bearing attribute
+			#lastTwo = item[len(item)-2:len(item)]
+			lastTwo = mMat[i]
+			if lastTwo == '00': d = '10'
+			elif lastTwo == '01': d = '12'
+			elif lastTwo == '02': d = '15'
+			elif lastTwo == '03': d = '17'
+			else:
+				d = lastTwo
+				print(d)
+				d = int(d)
+				d = d * 5
 
-		if len(mMat) = 4:
-			lastTwo = right(mMat, 2)
-			if lastTwo == '00': d = 10
-			if lastTwo == '01': d = 12
-			if lastTwo == '02': d = 15
-			if lastTwo == '03': d = 17
-			else
-			    d = int(lastTwo) * 5
-		att_str = str(lastTwo) + ' mm'
-
-
-		if len(mMat) = 5:
+			bearing_attr = str(d) + ' mm'
+	i += 1
 
 	# end program
 	print('Done.')
