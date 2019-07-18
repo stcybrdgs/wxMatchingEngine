c2535fc21536168c0633058efd2d4469ff9485b4
Stacy Bridges <stcybrdgs@gmail.com>
2019-07-17 20:56:49 -0500

    git commit

--- store/model/brmr_erp1/bearing_decoder_api_wlist.py
+++ store/model/brmr_erp1/bearing_decoder_api_wlist.py
@@ -41,70 +41,73 @@ def main():
 	attr_str =''  # use to collect api text that will be inserted into attributes[]
 	row_str = ''  # use to collect api text that will be inserted into rows_for_csv[]
 
-	# populate headers for csv file
-	global row_heads
-	i = 0
-	for row in row_heads:
-		if i == 0:
-			row_str = row_str + row_heads[i]
-		else:
-			row_str = row_str + '|' + row_heads[i]
-		i += 1
-	rows_for_csv.append([row_str])
-	print(row_str, '---------')
-	row_str = ''
-
-	# get codes as doc obj from external file
-	input_file = 'bearing_decoder_input.csv'
-	codes = import_csv(input_file)
-
-	# set api-endpoint for SKF
-	URL = "https://search.skf.com/prod/search-skfcom/rest/apps/site_search/searchers/products"
-
-	i = 0
-	for code in mMats:
-		# sending get request and saving the response as response object
-		PARAMS = {'q':code}
-		r = requests.get(url = URL, params = PARAMS)
-
-		# extracting data in json format
-		data = r.json()
-
-		# get description (ie, designation and category)
-		designation = data['documentList']['documents'][0]['designation']
-		category = data['documentList']['documents'][0]['category']
-
-		# add them to the csv string
-		row_str = row_str + designation + '|' + category + '|'
-
-		# get bearing attributes
-		search_result = data['documentList']['documents'][0]['search_result_nested']
-		j = 0
-		for item in search_result:
-			name = search_result[j]['name']
-			value = search_result[j]['value']
-			unit = search_result[j]['unit']
-			if j > 0:
-				attr_str = attr_str + ', '
-			attr_str = attr_str + name + ': ' + str(value) + ' ' + unit
-			j += 1
-
-		# add the attributes to the csv string
-		row_str = row_str + attr_str
-		print(row_str)
-
-		# store the string in a list so that it can be added to the csv
-		rows_for_csv.append([row_str])
-
-		# reset strings
-		row_str = ''
-		attr_str = ''
-		i+= 1
-
-	# write data to csv file
 	with open('bearing_decoder_output.csv', 'w') as writeFile:
 		writer = csv.writer(writeFile)
-		writer.writerows(rows_for_csv)
+
+		# populate headers for csv file
+		global row_heads
+		i = 0
+		for row in row_heads:
+			if i == 0:
+				row_str = row_str + row_heads[i]
+			else:
+				row_str = row_str + '|' + row_heads[i]
+			i += 1
+		rows_for_csv.append([row_str])
+		print(row_str, '---------')
+		row_str = ''
+
+		# get codes as doc obj from external file
+		input_file = 'bearing_decoder_input.csv'
+		codes = import_csv(input_file)
+
+		# set api-endpoint for SKF
+		URL = "https://search.skf.com/prod/search-skfcom/rest/apps/site_search/searchers/products"
+
+		i = 0
+		for code in mMats:
+			# sending get request and saving the response as response object
+			PARAMS = {'q':code}
+			r = requests.get(url = URL, params = PARAMS)
+
+			# extracting data in json format
+			data = r.json()
+
+			# get description (ie, designation and category)
+			designation = data['documentList']['documents'][0]['designation']
+			category = data['documentList']['documents'][0]['category']
+
+			# add them to the csv string
+			row_str = row_str + designation + '|' + category + '|'
+
+			# get bearing attributes
+			search_result = data['documentList']['documents'][0]['search_result_nested']
+			j = 0
+			for item in search_result:
+				name = search_result[j]['name']
+				value = search_result[j]['value']
+				unit = search_result[j]['unit']
+				if j > 0:
+					attr_str = attr_str + ', '
+				attr_str = attr_str + name + ': ' + str(value) + ' ' + unit
+				j += 1
+
+			# add the attributes to the csv string
+			row_str = row_str + attr_str
+			print(row_str)
+
+			# store the string in a list so that it can be added to the csv
+			rows_for_csv.append([row_str])
+
+			# reset strings
+			row_str = ''
+			attr_str = ''
+			i+= 1
+
+		# write data to csv file
+		with open('bearing_decoder_output.csv', 'w') as writeFile:
+			writer = csv.writer(writeFile)
+			writer.writerows(rows_for_csv)
 
 	# end program
 	print('Done.')
