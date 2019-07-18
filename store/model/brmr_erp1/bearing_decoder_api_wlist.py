571ed0d1d9e8cbce2eba0548b5e483a4f86665e2
Stacy Bridges <stcybrdgs@gmail.com>
2019-07-17 19:08:35 -0500

    git commit

--- store/model/brmr_erp1/bearing_decoder_api_wlist.py
+++ store/model/brmr_erp1/bearing_decoder_api_wlist.py
@@ -41,8 +41,11 @@ def import_csv(d):
 
 # MAIN  ------------------------------------------------------------
 def main():
-	descriptions = []
-	attributes = []
+	#descriptions = []
+	attributes = []  # each index contains a product attribute
+	rows_for_csv = []  # each index contains a row that will be written to the csv
+	attr_str =''
+	row_str = ''
 	input_file = 'bearing_decoder_input.csv'
 
 	# get codes as doc obj from external file
@@ -51,48 +54,71 @@ def main():
 	# set api-endpoint for SKF
 	URL = "https://search.skf.com/prod/search-skfcom/rest/apps/site_search/searchers/products"
 
+	#with open('bearing_decoder_output.csv', 'w') as writeFile:
+	#	writer = csv.writer(writeFile)
+
+	i = 0
+	for code in mMats:
+		# sending get request and saving the response as response object
+		PARAMS = {'q':code}
+		r = requests.get(url = URL, params = PARAMS)
+
+		# extracting data in json format
+		data = r.json()
+
+		# get description (ie, designation and category)
+		designation = data['documentList']['documents'][0]['designation']
+		category = data['documentList']['documents'][0]['category']
+		row_str = row_str + designation + '|' + category + '|'
+		#descriptions.append([designation, category])
+		#rows_for_csv.append([designation])
+		#rows_for_csv.append('|')
+		#rows_for_csv.append([category])
+
+		# get attributes
+		search_result = data['documentList']['documents'][0]['search_result_nested']
+		j = 0
+		for item in search_result:
+			name = search_result[j]['name']
+			value = search_result[j]['value']
+			unit = search_result[j]['unit']
+			#attributes.append([name, value, unit])
+			if j > 0:
+				attr_str = attr_str + ', '
+			attr_str = attr_str + name + ': ' + str(value) + ' ' + unit
+
+			# write the attributes to rows_for_csv[]
+			#rows_for_csv.append([attributes])
+			#writer.writerows(rows_for_csv)
+
+			#print(rows_for_csv)
+			#print(attributes)
+
+			#attributes.clear()  # clear attributes array for next pass
+			#rows_for_csv.clear()
+
+			j += 1
+
+		row_str = row_str + attr_str
+		print(row_str)
+		row_str = ''
+		attr_str = ''
+		i+= 1
+
+	#writer.writerows(row_str)
+
+
+	#with open('bearing_decoder_output.txt', 'w') as of:
+	#	print(row_str, file=of)
+
+
+
+	'''
 	with open('bearing_decoder_output.csv', 'w') as writeFile:
 		writer = csv.writer(writeFile)
+		writer.writerows()
+	'''
 
-		i = 0
-		for code in mMats:
-			# sending get request and saving the response as response object
-			PARAMS = {'q':code}
-			r = requests.get(url = URL, params = PARAMS)
-
-			# extracting data in json format
-			data = r.json()
-
-			# get description (ie, designation and category)
-			designation = data['documentList']['documents'][0]['designation']
-			category = data['documentList']['documents'][0]['category']
-			descriptions.append([designation, category])
-
-			print(designation, '|', end='')
-			print(category, '|', end='')
-
-			# get attributes
-			search_result = data['documentList']['documents'][0]['search_result_nested']
-			j = 0
-			for item in search_result:
-				attributes.append({
-					search_result[j]['name'],
-					search_result[j]['value'],
-					search_result[j]['unit']
-				})
-				print({
-					search_result[j]['name'],
-					search_result[j]['value'],
-					search_result[j]['unit']
-				}, end='')
-				j += 1
-			print('')
-
-		'''
-		with open('bearing_decoder_output.csv', 'w') as writeFile:
-			writer = csv.writer(writeFile)
-			writer.writerows()
-		'''
 
 	# end program
 	print('Done.')
