78d05dd0dbb85f8a6759417a77783f2ffd05a79c
Stacy Bridges <stcybrdgs@gmail.com>
2019-08-07 18:30:34 -0500

    git commit

---
+++ store/model/brmr_erp1/nu_demo/flask_rest_api_caller.py
@@ -0,0 +1,31 @@
+import requests
+
+# set api-endpoint for local flask rest api
+URL = "http://127.0.0.1:5000/user/"
+
+names = ['Stacy', 'Anushree', 'Betty', 'Eric', 'Ron']
+
+for name in names:
+    # send get request and save response as response object
+    #PARAMS = 'Stacy'
+    r = requests.get(url = URL + name)#, params = PARAMS)
+    # extracting data in json format
+    data = r.json()
+
+    try:
+        name = data['name']
+    except:
+        name = 'none'
+    try:
+        role = data['role']
+    except:
+        role = 'none'
+    try:
+        age = data['age']
+    except:
+        age = 'none'
+
+    if name == 'none':
+        print('n/a')
+    else:
+        print('Name: {}, Role: {}, Age: {}'.format(name, role, age))
