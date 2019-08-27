c38420cb8675589e963c05f96a7d1c73c80f4526
Stacy Bridges <stcybrdgs@gmail.com>
2019-08-27 14:18:36 -0500

    git commit

--- store/model/brmr_erp1/eriks/test/matcher_tester.py
+++ store/model/brmr_erp1/eriks/test/matcher_tester.py
@@ -1,15 +1,75 @@
 import spacy
 from spacy.matcher import Matcher
+from spacy.pipeline import EntityRuler
+from spacy.matcher import PhraseMatcher
 
-nlp = spacy.load("en_core_web_sm")
-matcher = Matcher(nlp.vocab)
-# Add match ID "HelloWorld" with no callback and one pattern
-pattern = [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}]
-matcher.add("HelloWorld", None, pattern)
+"""
+from spacy.matcher import PhraseMatcher
 
-doc = nlp(u"Hello, world! Hello world!")
+matcher = PhraseMatcher(nlp.vocab)
+matcher.add("OBAMA", None, nlp(u"Barack Obama"))
+doc = nlp(u"Barack Obama lifts America one last time in emotional farewell")
 matches = matcher(doc)
+
+----------------------
+
+snippets:
+{"label":"MPN", "pattern":[{"lower":"07342e-10"}]}
+nu_ruler = EntityRuler(nlp).from_disk(patterns_file)
+nlp.add_pipe(nu_ruler)#, before='ner')
+
+ruler = EntityRuler(nlp)
+ruler.add_patterns([{"label": "ORG", "pattern": "Apple"}])
+nlp.add_pipe(ruler)
+
+doc = nlp("A text about Apple.")
+ents = [(ent.text, ent.label_) for ent in doc.ents]
+assert ents == [("Apple", "ORG")]
+
+
+ruler = EntityRuler(nlp)
+ruler.add_patterns(patterns)
+
+nlp = spacy.load("en_core_web_sm", disable=['ner'])
+ruler = EntityRuler(nlp, overwrite_ents=True)
+ruler.add_patterns(patterns)
+nlp.add_pipe(ruler)
+
+"""
+# load model
+nlp = spacy.load("en_core_web_sm")
+
+# create matcher
+matcher = Matcher(nlp.vocab)
+
+# define patterns for the matcher
+hello_patterns = [
+    [{"LOWER": "hello"}, {"IS_PUNCT": True}, {"LOWER": "world"}],
+    [{"LOWER": "hello"}, {"LOWER": "world"}]
+]
+
+# add patterns to the matcher
+for item in hello_patterns:
+    matcher.add("HelloWorld", None, item)
+
+# create nlp document
+doc = nlp(u"Hello, world! Hello world!")
+
+# find strings in nlp doc that match patterns in the matcher
+matches = matcher(doc)
+
+# for matches, print out info
 for match_id, start, end in matches:
     string_id = nlp.vocab.strings[match_id]  # Get string representation
     span = doc[start:end]  # The matched span
     print(match_id, string_id, start, end, span.text)
+
+print('sent.text, sent.pos_: ------------')
+for sent in doc:
+    print(sent.text, sent.pos_)
+
+print('ent.text, ent.label_: ------------')
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+
+print('Done.')
