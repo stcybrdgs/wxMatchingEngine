0a45925b14285615ceeb42e3a0c6d1f451b91b45
Stacy Bridges <stcybrdgs@gmail.com>
2019-10-24 14:38:52 -0500

    git commit

--- nu_ners/add_wx_cols.py
+++ nu_ners/add_wx_cols.py
@@ -17,7 +17,7 @@ https://xlsxwriter.readthedocs.io/
 
 """
 # IMPORTS  ---------------------------------------------------------------------
-import os
+import os, shutil, sys
 import pathlib
 from pathlib import Path
 import shutil
@@ -29,59 +29,130 @@ import numpy as np
 import xlsxwriter
 from xlsxwriter.utility import xl_rowcol_to_cell  # excel formatting
 
-# start program
-print('Adding wx fields...')
+# import py files  -------------------------------------------------------------
+import menu
 
-# define paths/names for files and directories
-mkpath = os.path.abspath(__file__)  # = (os.path.abspath('.'))
-projectDir = os.path.dirname(mkpath)
-templateDir = os.path.dirname(projectDir) + r'\NERS_Template'  # relative path
-nersDir = projectDir + r'\ners'
-projectFile = ''
-projectFilename = ''
-projectSheetName = ''
-metaDir = projectDir + r'\input'  # relative path
-metaFilename = r'\in_wxmeta.xlsx'
-metaFile = metaDir + metaFilename
-wxFile = ''
-wxFilename = ''
-wxSheetName = 'WrWx Fields'
-numberOfRows = 0
-hashids = Hashids(alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', min_length=5)
+def main():
+    # print menu options to console  -----------------------------------------------
+    # declare menu and file arrays
+    menu_choices = []
+    file_choices = []
+    console_message = ''
 
-# get the name of the project file
-count_xlsx = 0
-for item in os.listdir(projectDir):
-    name = os.path.splitext(item)[0]
-    ext = os.path.splitext(item)[1]
-    if count_xlsx > 1:
-        # throw an error if there is more than one project file
-        print('Error: There can be only one .xlsx file at the base of {}'.format(projectDir))
-        break
-    if ext == '.xlsx':
-        count_xlsx += 1
-        # name the project file
-        projectFilename = name + ext
-        projectFile = projectDir + '\\' + projectFilename
-        # name the wx outfile
-        wxFilename = name + '_wx_v1' + ext
-        wxFile = projectDir + '\\' + wxFilename
+    # get path of current folder
+    folder_path = os.path.dirname(os.path.abspath(__file__))
 
-'''
-print('mkpath:', mkpath)
-print('projectDir:',projectDir)
-print('templateDir:',templateDir)
-print('nersDir:',nersDir)
-print('wxFilename:', wxFilename)
-print('wxFile:', wxFile)
-'''
+    # get names of .xlsx files that are in the folder that are also input files
+    for r, d, f in os.walk(folder_path):  # rem r=root, d=dir, f=file
+        for file in f:
+            if '.xlsx' in file and 'data' in file and '_wx_v1' not in file:
+                # rem for full path use <files.append(os.path.join(r, file))>
+                file_choices.append(file)
 
-# only create transformed wx file if exactly one project file exists in folder
-if count_xlsx <= 1:
-    if count_xlsx == 0:
-        # throw an error if there is no project file
-        print('Error: There must be one .xlsx file at the base of {}'.format(projectDir))
+    # print user menu
+    print('\n-----------------------------------------')
+    print('           Data Files')
+    print('-----------------------------------------')
+    spacer ='  '
+    print('{}{}{}'.format('m', spacer, 'Show Main Menu'))
+    menu_choices.append('m')
+    i = 0
+    for ic in file_choices:
+        i += 1
+        print('{}  {}'.format(i, ic))
+        menu_choices.append(str(i))
+
+    # get user input
+    if len(file_choices) == 0:
+        console_message = '\nNo data files available. Select \'m\' for Main Menu'
     else:
+        console_message = '\nSelect a data file (or \'m\' for Main Menu)'
+    print(console_message)
+    user_choice = input()
+
+    # validate user input
+    while user_choice not in menu_choices:
+        print('Invalid choice! {}'.format(console_message))
+        user_choice = input()
+
+    if user_choice == 'm':
+        menu.main()
+
+        # if the user chooses 'm', then program control goes back to menu.main(),
+        # which means that when menu.main() terminates, the program control will
+        # return to this program; therefore, it's important to invoke sys.exit()
+        # upon the callback to terminate all py execution in the terminal
+        sys.exit()
+
+    # see if user-selected file has already been made into a wx_v1 file
+    user_selected_file = file_choices[int(user_choice)-1]
+    wx_files = []
+    wx_file_exists = False
+    # find wx_v1 filenames and put them in array
+    for r, d, f in os.walk(folder_path):  # rem r=root, d=dir, f=file
+        for file in f:
+            if 'xlsx' in file and 'wx_v1' in file:
+                wx_files.append(file)
+
+    for f in wx_files:
+        if user_selected_file[0:len(user_selected_file)-5] in f:
+            wx_file_exists = True
+
+    # if the wx_v1 file already exists, see if user wants to overwrite it
+    want_to_add_cols = True
+    if wx_file_exists:
+        print('A \'wx_v1\' file already exists for that data. Overwrite (y/n)?')
+        yn_choice = input()
+        while yn_choice not in ['y', 'n', 'Y', 'N']:
+            print('Invalid choice! Overwrite (y/n)?')
+            yn_choice = input()
+        if yn_choice == 'n':
+            want_to_add_cols = False
+
+    # if the user wants to overwrite the existing wx_1 file, or
+    # if there is not yet a wx_1 file for the user-selected data file, then
+    # create the new wx_1 file
+    if want_to_add_cols:
+        print('\nAdding wx columns to \'' + user_selected_file + '\' ...')
+
+        # define paths/names for files and directories
+        mkpath = os.path.abspath(__file__)  # = (os.path.abspath('.'))
+        projectDir = os.path.dirname(mkpath)
+        #templateDir = os.path.dirname(projectDir) + r'\NERS_Template'  # relative path
+        #nersDir = projectDir + r'\ners'
+        #projectFile = ''
+        #projectFilename = ''
+        projectSheetName = ''
+        metaDir = projectDir + r'\input'  # relative path
+        metaFilename = r'\in_wxmeta.xlsx'
+        metaFile = metaDir + metaFilename
+        wxFile = ''
+        wxFilename = ''
+        wxSheetName = 'WrWx Fields'
+        numberOfRows = 0
+        hashids = Hashids(alphabet='ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', min_length=5)
+
+        # get the name of the project file
+        wxFilename = user_selected_file[0:len(user_selected_file)-5] + '_wx_v1.xlsx'
+        wxFile = projectDir + '\\' + wxFilename
+        '''
+        count_xlsx = 0
+        for item in os.listdir(projectDir):
+            name = os.path.splitext(item)[0]
+            ext = os.path.splitext(item)[1]
+            if count_xlsx > 1:
+                # throw an error if there is more than one project file
+                print('Error: There can be only one .xlsx file at the base of {}'.format(projectDir))
+                break
+            if ext == '.xlsx':
+                count_xlsx += 1
+                # name the project file
+                projectFilename = name + ext
+                projectFile = projectDir + '\\' + projectFilename
+                # name the wx outfile
+                wxFilename = name + '_wx_v1' + ext
+                wxFile = projectDir + '\\' + wxFilename
+        '''
         # declare arrays to hold data from the project and metadata files
         pf_headers = []
         pf_cols = []
@@ -90,14 +161,14 @@ if count_xlsx <= 1:
         wx_hashIds = []
 
         # read the project and metadata files into dataframes
-        df_pf = pd.read_excel(projectFile, sheet_name=0)
+        df_pf = pd.read_excel(user_selected_file, sheet_name=0)
         df_meta = pd.read_excel(metaFile, sheet_name=1)
 
         # get the number of records in project files
         numberOfRows = len(df_pf.index)
 
         # get name of worksheet in the project file
-        pf = pd.ExcelFile(projectFile)
+        pf = pd.ExcelFile(user_selected_file)
         projectSheetName = pf.sheet_names[0]
 
         # copy the file headers to arrays
@@ -139,6 +210,10 @@ if count_xlsx <= 1:
         # create pandas data frames
         df_main = pd.DataFrame(main_dict)  # main worksheet
 
+        print('TEST -- HERE --------------')
+        print(wxFilename)
+        print(wxFile)
+
         # create excel writer
         writer = pd.ExcelWriter(wxFile)
 
@@ -147,19 +222,7 @@ if count_xlsx <= 1:
         df_main.to_excel(writer, projectSheetName, index=False)
         writer.save()
 
-        # test  ----------------------------------------------------------------
-        #'''
-        print('mkpath:\t\t\t{}\nprojectDir:\t\t{}\nnersDir:\t\t{}\ntemplateDir:\t\t{}\nmetaDir:\t\t{}\nmetaFile:\t\t{}\nprojectFile:\t\t{}\nprojectFilename:\t{}\nwxFile:\t\t\t{}'.format(
-            mkpath, projectDir, nersDir, templateDir, metaDir, metaFile, projectFile, projectFilename, wxFile))
-        print('pf_headers:', pf_headers)
-        print('wx_headers:', wx_headers)
-        print('projectSheetName:', projectSheetName)
-        print('pf_headers:', pf_headers)
-        print('wx_headers:', wx_headers)
-        print('number of rows:', numberOfRows)
-        print('number of pf headers:', len(pf_headers))
-        #'''
+        # end program
+        print('File complete:', wxFile)
 
-# end program
-print('File complete:', wxFile)
-print('Done.')
+if __name__ == '__main__' : main()
