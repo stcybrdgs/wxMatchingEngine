e183caa9ec9f5c95cc89515d0a4ed07a9413e6de
Stacy Bridges <stcybrdgs@gmail.com>
2019-11-05 14:54:58 -0600

    git commit

--- nu_ners/tbd.py
+++ nu_ners/tbd.py
@@ -1,5 +1,8 @@
+import generate_mpn_patterns_2
 
 def main():
     print('\nThis function is under development.')
+
+    generate_mpn_patterns_2.main('db_mpn_delme_test')
 
 if __name__ == '__main__' : main()
