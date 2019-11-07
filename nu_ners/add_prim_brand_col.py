51b0528537cc4142212c4ad827fa117fc21d7d24
Stacy Bridges <stcybrdgs@gmail.com>
2019-11-07 15:16:06 -0600

    git commit

--- nu_ners/add_prim_brand_col.py
+++ nu_ners/add_prim_brand_col.py
@@ -10,20 +10,31 @@ Selects the primary brand for each record:
 - if wBrand_all count > 1:
     - for each brand b in the wBrand_all field, count each time it occurs in the column
     - then, primary brand == max [b1_count, b2_count, ..., bn_count]
-- if wBrand_prim column was previously populated, overwrite it each time new values
+- if wBrand_primary column was previously populated, overwrite it each time new values
   are determined
 
 """
+# library imports  -------------------------------------------------------------
 import pandas as pd
 from pandas import ExcelWriter
 import numpy as np
 
+# helper functions -------------------------------------------------------------
+def show_brand_frequency(unique_brands, unique_brands_counts):
+    i = 0
+    for ub in unique_brands:
+        ubcount = unique_brands_counts[i]
+        print('{} | {}'.format(ub, ubcount))
+        i += 1
+
 def main():
-    user_selected_file = r'C:\Users\stacy\Desktop\IESA Project - Europe\IESA Phase 2\ners_v2\ners\db_data_org_electrical_short_wx_v1.xlsx'
+    # main  ------------------------------------------------------------------------
+    file_name = r'db_data_org_electrical_short_wx_v1.xlsx'
+    file_path = r'C:\Users\stacy\Desktop\IESA Project - Europe\IESA Phase 2\ners_v2\ners\db_data_org_electrical_short_wx_v1.xlsx'
 
     # read the project and metadata files into dataframes
-    df = pd.read_excel(user_selected_file, sheet_name=0)
-    wBrand_all = df['wBrand_all']
+    df_data = pd.read_excel(file_path, sheet_name=0)
+    wBrand_all = df_data['wBrand_all']
 
     # store each wBrand_all record as a string in a new array
     brand_rows = []
@@ -80,38 +91,51 @@ def main():
                 i += 1
 
     # get the primary brand for each record
-    wBrand_prim = []
+    wBrand_primary = []
     brand_names_in_list = []
     brand_counts_in_list = []
     for list in brand_rows:
-        j = 0
-        for item in list:
-            k = 0:
-            for unique_item in unique_brands:
-                if item == unique_item:
-                    brand_names_in_list.append(item)
-                    brand_counts_in_list.append(unique_brands_counts[k])
-                    break
-                k += 1  # inc unique_item in unique_brands
-            j += 1  # inc item in list
-        # maxval = max val in brand_counts_in_list
-        # index = index of max val in brand_counts_in_list
-        # wBrand_prim.append(brand_names_in_list[index])
+        if len(list) > 0:
+            j = 0
+            for item in list:
+                k = 0
+                for unique_item in unique_brands:
+                    if item == unique_item:
+                        brand_names_in_list.append(item)
+                        brand_counts_in_list.append(unique_brands_counts[k])
+                        break
+                    k += 1  # inc unique_item in unique_brands
+                j += 1  # inc item in list
+            # get index of brand that has the highest number of occurrences,
+            # but if there is a tie, default to the first of the tied brands
+            index_of_max_count = brand_counts_in_list.index(max(brand_counts_in_list))
+            prim_brand = brand_names_in_list[index_of_max_count]
+            wBrand_primary.append(prim_brand)
+        else:
+            wBrand_primary.append('')
+        brand_names_in_list.clear()  # clear array for next iteration
+        brand_counts_in_list.clear()  # clear array for next iteration
 
-    # prepare pandas data frame for output file where
-    # {'wBrand_prim':wBrand_prim}
+    # write data to project file
+    p_dict = {}
+    for head in df_data:
+        if head == 'wBrand_primary':
+            p_dict.update({head:wBrand_primary})
+        else:
+            p_dict.update({head:df_data[head]})
 
-
-
-    # TEST  --------------------------------------------------------------------
-    print('{} total, {} unique'.format(brand_tot_count, len(unique_brands)))
-    i = 0
-    for ub in unique_brands:
-        ubcount = unique_brands_counts[i]
-        print('{} | {}'.format(ub, ubcount))
-        i += 1
+    df_out = pd.DataFrame(p_dict)
+    writer = pd.ExcelWriter(file_path)
+    df_out.to_excel(writer, 'TestData', index=False)
+    writer.save()
 
     # end program
+    # show a list of brand frequency
+    #show_brand_frequency(unique_brands, unique_brands_counts)
+    print('Primary brands written')
+    print('{} total, {} unique'.format(brand_tot_count, len(unique_brands)))
+    print(file_name)
+    print(file_path)
     print('Done.')
 
 if __name__ == '__main__' : main()
