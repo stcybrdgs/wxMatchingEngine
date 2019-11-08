364595aacacabcd37eafdf4a46a8995675022a51
Stacy Bridges <stcybrdgs@gmail.com>
2019-11-07 20:27:34 -0600

    git commit

--- nu_ners/regex_test.py
+++ nu_ners/regex_test.py
@@ -11,29 +11,48 @@ import numpy as np
 
 # get input file
 folder_path = os.path.dirname(os.path.abspath(__file__))
-infile = folder_path + '\\db_data_org_electrical_short_wx_v1.xlsx'
+infile = folder_path + '\\db_data_org_electrical_nutest_wx_v1.xlsx'
 outfile = folder_path + '\\api_in_ids.csv'
 
 # get dataframe/column
 df_tender = pd.read_excel(infile, sheet_name=0)  # read file into dataframe
-mpns = df_tender['ManufacturerPartNo']
+mpns = df_tender['Description']  #mpns = df_tender['ManufacturerPartNo']
 
-# identify search pattern
-RS_regex='\d{3}[-]\d{3,4}'
+# identify regex search patterns
+rs_regex_1='[\s,({[]\d{3}[-]\d{3,4}[\s,)}]'  # case 1: the regex is inline
+rs_regex_2='^\d{3}[-]\d{3,4}[\s,)}]'  # case 2: the regex is at start of line
+rs_regex_3='[\s,({[]\d{3}[-]\d{3,4}$'  # case 3: the regex is at end of line
 
-# print search results to console
+# find rs codes per regex rules
 results = []
 for row in mpns:
-    results.append(re.findall(RS_regex,str(row)))
+    results.append(re.findall(rs_regex_3,str(row)))
+
+# strip leading and trailing chars from result strings
+lead_trail_chars = [' ', ',', '(', '{', '[', ')', '}', ']']
+final_codes = []
+for list in results:
+    if len(list) > 0:
+        for string in list:
+            token = ''
+            #if len(string) > 0:
+            for char in string:
+                if char not in lead_trail_chars:
+                    token = token + char
+            final_codes.append(token)
+    else:
+        final_codes.append('')
+
+# print final codes to outfile and console
 with open(outfile, 'w') as ofile:
     ofile.write('RS Code')
     ofile.write('\n')
     id_count = 0
-    for i in results:
-        if len(i)>0:
+    for item in final_codes:
+        if len(item)>0:
             id_count += 1
-            print(i[0])
-            ofile.write(i[0])
+            print(item)
+            ofile.write(item)
             ofile.write('\n')
         else:
             ofile.write('\n')
